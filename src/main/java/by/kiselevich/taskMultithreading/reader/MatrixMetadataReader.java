4af3bc2ba4fd868f74251f2ec17576fa6df30518
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-11 10:24:21 +0300

    Fix MatrixMetadataReader, replace String path to File file field, add some log traces

--- src/main/java/by/kiselevich/taskMultithreading/reader/MatrixMetadataReader.java
+++ src/main/java/by/kiselevich/taskMultithreading/reader/MatrixMetadataReader.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -12,17 +13,19 @@ public class MatrixMetadataReader {
     private static final Logger LOG = LogManager.getLogger(MatrixMetadataReader.class);
     private static final int N_INDEX = 0;
     private static final int Y_INDEX = 1;
+    private static final int DATA_ARRAY_LENGTH = 2;
 
-    private String path;
+    private File file;
     private int n;
     private int y;
 
-    public MatrixMetadataReader(String path) {
-        this.path = path;
+    public MatrixMetadataReader(File file) {
+        this.file = file;
         readTwoNumbers();
     }
 
-    public void setPath(String path) {
+    public void setPath(File file) {
+        this.file = file;
         readTwoNumbers();
     }
 
@@ -35,19 +38,25 @@ public class MatrixMetadataReader {
     }
 
     private void readTwoNumbers() {
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line = br.readLine();
-            if (line != null) {
-                String[] numbersStringArray = line.split(";");
-                if (numbersStringArray.length >= 2) {
-                    n = Integer.parseInt(numbersStringArray[N_INDEX]);
-                    y = Integer.parseInt(numbersStringArray[Y_INDEX]);
-                    return;
+        if (file != null) {
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                if (line != null) {
+                    String[] numbersStringArray = line.split(";");
+                    if (numbersStringArray.length >= DATA_ARRAY_LENGTH) {
+                        n = Integer.parseInt(numbersStringArray[N_INDEX]);
+                        y = Integer.parseInt(numbersStringArray[Y_INDEX]);
+                    } else {
+                        LOG.warn("Cant read matrix metadata, string = <" + line + ">");
+                    }
+                } else {
+                    LOG.warn("Matrix metadata is empty");
                 }
+            } catch (IOException e) {
+                LOG.warn(e);
             }
-            LOG.warn("Cant read numbers, string = <" + line + ">");
-        } catch (IOException e) {
-            LOG.warn(e);
+        } else {
+            LOG.warn("No file to read matrix metadata");
         }
     }
 }
