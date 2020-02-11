6c7263becd237403876a36cac8da09acda62a58d
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-11 11:03:12 +0300

    Add input and output files init, add output data writing

--- src/main/java/by/kiselevich/taskMultithreading/runner/Runner.java
+++ src/main/java/by/kiselevich/taskMultithreading/runner/Runner.java
@@ -2,16 +2,42 @@ package by.kiselevich.taskMultithreading.runner;
 
 import by.kiselevich.taskMultithreading.constant.ConstantValues;
 import by.kiselevich.taskMultithreading.entity.Matrix;
+import by.kiselevich.taskMultithreading.thread.MatrixAndThreadSumWriterThread;
 import by.kiselevich.taskMultithreading.thread.MatrixChangerThread;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class Runner {
     private final static Logger LOG = LogManager.getLogger(Runner.class);
+    private final static String OUTPUT_FILE = "output.txt";
+    private final static String INPUT_FILE = "input.txt";
+
+    private static File outputFile = new File(OUTPUT_FILE);
+    private static File inputFile = null;
+    static {
+        try {
+            if (outputFile.exists()) {
+                outputFile.delete();
+            }
+            outputFile.createNewFile();
+        } catch (IOException e) {
+            LOG.warn(e);
+        }
+
+        URL inputFileUrl = Runner.class.getClassLoader().getResource(INPUT_FILE);
+        if (inputFileUrl != null) {
+            inputFile = new File(inputFileUrl.getFile());
+        } else {
+            LOG.warn("No input file for matrix metadata");
+        }
+    }
 
     public static void main(String[] args) {
-        Thread[] threads = new Thread[ConstantValues.N];
+        MatrixChangerThread[] threads = new MatrixChangerThread[ConstantValues.N];
         int id = 0;
         for (int i = 0; i < ConstantValues.Y; i++) {
             for (int j = 0; j < ConstantValues.N; j++) {
@@ -29,6 +55,13 @@ public class Runner {
             }
             LOG.trace(Matrix.getInstance().toString());
             Matrix.getInstance().resetUseControl();
+            Thread writer = new MatrixAndThreadSumWriterThread(outputFile, threads);
+            writer.start();
+            try {
+                writer.join();
+            } catch (InterruptedException e) {
+                LOG.warn(e);
+            }
         }
 
     }
