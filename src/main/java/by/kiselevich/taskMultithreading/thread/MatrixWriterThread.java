82f123e611603a9d0e52be232333fd256763b0b5
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-11 09:50:17 +0300

    Add MatrixWriterThread

---
+++ src/main/java/by/kiselevich/taskMultithreading/thread/MatrixWriterThread.java
@@ -0,0 +1,36 @@
+package by.kiselevich.taskMultithreading.thread;
+
+import by.kiselevich.taskMultithreading.entity.Matrix;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MatrixWriterThread extends Thread {
+
+    private static final Logger LOG = LogManager.getLogger(MatrixWriterThread.class);
+
+    private File file;
+
+    public MatrixWriterThread(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public void run() {
+        Matrix matrix = Matrix.getInstance();
+
+        if (file != null) {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+                bufferedWriter.write(matrix.toString());
+            } catch (IOException e) {
+                LOG.warn(e);
+            }
+        } else {
+            LOG.warn("No file to write matrix");
+        }
+    }
+}
